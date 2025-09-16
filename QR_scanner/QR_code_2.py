5e4a452db9ad73b177474e198497959a0f6d2ec5
Nguyễn Huy Chương <74437696+medalnguyen@users.noreply.github.com>
2025-09-16 11:14:26 +0700

    Thêm requirement.txt và chỉnh sửa tên file

--- QR_scanner/QR_code_2.py
+++ QR_scanner/QR_code_2.py
@@ -1,31 +1,22 @@
 from pdf2image import convert_from_path
 import cv2
 import pyzbar.pyzbar as pyzbar
-import os
+# B1: Chuyển từng trang PDF thành ảnh
+pages = convert_from_path(r"E:\Promgramming\BB_AQAT-94_AQAT-94-01_S-CC-DAC.pdf", dpi=300, poppler_path=r"C:\poppler-25.07.0\Library\bin")
 
-def extract_qr_from_pdf(pdf_paths):
-    for pdf_path in pdf_paths:
-        print(f"Processing file: {pdf_path}")
-        page_number = 1
-        for page in convert_from_path(pdf_path, dpi=300, poppler_path=r"C:\poppler-25.07.0\Library\bin"):
-            # Lưu tạm thành file ảnh
-            filename = "temp_page.jpg"
-            page.save(filename, "JPEG")
+for i, page in enumerate(pages):
+    # Lưu tạm thành file ảnh
+    filename = f"page_{i}.jpg"
+    page.save(filename, "JPEG")
 
-            # Đọc lại ảnh bằng OpenCV
-            img = cv2.imread(filename)
+    # Đọc lại ảnh bằng OpenCV
+    img = cv2.imread(filename)
 
-            # Giải mã QR trong ảnh
-            decoded_objects = pyzbar.decode(img)
+    # B2: Giải mã QR trong ảnh
+    decoded_objects = pyzbar.decode(img)
 
-            # Xuất kết quả
-            for obj in decoded_objects:
-                print(f"Page: {page_number}")
-                print("Type:", obj.type)
-                print("Data:", obj.data.decode("utf-8"))
-            page_number += 1
-
-if __name__ == "__main__":
-    folder_path = r"D:\Promgramming\QR_path"  # Thay đổi đường dẫn tới thư mục chứa các file PDF
-    pdf_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.lower().endswith('.pdf')]
-    extract_qr_from_pdf(pdf_paths)
+    # B3: Xuất kết quả
+    for obj in decoded_objects:
+        print("Page:", i+1)
+        print("Type:", obj.type)
+        print("Data:", obj.data.decode("utf-8"))
